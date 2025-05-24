f49e750c932e295e91c1127d4f34453a93565bae
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-24 14:03:34 +0200

    last commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 import { authenticateUser } from '../utils/auth';
 const Login = () => {
   const [username, setUsername] = useState('');
