fafebe5c2001d2d5604c24621723786f0e607a52
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-24 14:03:13 +0200

    last commit

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 const Events = () => {
   const [events, setEvents] = useState([]);
   const [eventName, setEventName] = useState('');
